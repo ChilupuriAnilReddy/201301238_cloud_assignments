3c3036d12f69dcfcc93b18ca6695800eb3c0625e
ChilupuriAnilReddy <chilupuri.anilreddy@gmail.com>
2015-09-03 22:32:58 +0530

    updated

--- Assignment2/Readme.txt
+++ Assignment2/Readme.txt
@@ -3,3 +3,9 @@ TO Execute:
 2.. sudo python custom_topology.py
 3.. then enter number of switches required
 4.. then enter number of hosts required 
+
+output :
+ Shows result of pingall command
+ shows bandwidth between h3 and h1 if number of hosts are grater than 2
+ shows bandwidth between h3 and h1 if number of hosts are grater than 3
+
--- Assignment2/custom_topology.py
+++ Assignment2/custom_topology.py
@@ -4,6 +4,12 @@ from mininet.net import Mininet
 from mininet.util import dumpNodeConnections                                                                   
 from mininet.node import Controller, RemoteController
 from mininet.cli import CLI
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import CPULimitedHost
+from mininet.link import TCLink
+from mininet.util import dumpNodeConnections
+from mininet.log import setLogLevel
 import os
 class MyTopo( Topo ):
     def __init__( self,x,y):
@@ -25,23 +31,26 @@ class MyTopo( Topo ):
 
         for i in xrange(len(switch)):
             try:
-                self.addLink(switch[i],host[(2*i)+1])
+                self.addLink(host[(2*i)+1],switch[i],bw=1)
             except:
                 c = 1
             try:
-                self.addLink(switch[i],host[(2*i)])
+                self.addLink(host[(2*i)],switch[i],bw=2)
             except:
                 c=1
         if y>2*x:
             for i in xrange(2*x,y):
-                self.addLink(switch[x-1],host[i])
+                if (i+1)%2==0:
+                    self.addLink(host[i],switch[x-1],bw=2)
+                else:
+                    self.addLink(host[i],switch[x-1],bw=1)
 
 
 def  required(x,y):
     topo = MyTopo(x, y)
-    net = Mininet(topo)
+    net = Mininet(topo,host=CPULimitedHost, link=TCLink)
     net.start()
-    #net.addController('c0', controller=RemoteController,ip="127.0.0.1",port=6633)
+
     for i in xrange(y):
         for j in xrange(y):
             if (i+1)%2==0 and (j+1)%2==1:
@@ -50,8 +59,27 @@ def  required(x,y):
             elif (i+1)%2==1 and (j+1)%2==0:
                 net.nameToNode["h"+str(i+1)].cmd("iptables -A OUTPUT -o h"+str(i+1)+"-eth0 -d 10.0.0."+ str(j+1)+" -j DROP")
                 net.nameToNode["h"+str(j+1)].cmd("iptables -A OUTPUT -o h"+str(j+1)+"-eth0 -d 10.0.0."+ str(i+1)+" -j DROP")
+    
+    net.pingAll()
+    try:
+        
+        print "Testing bandwidth between h1 and h3"
+        h1, h3 = net.get('h1', 'h3')
+        net.iperf((h1, h3))
+    except:
+        c=1
+    try:
+        
+        print "Testing bandwidth between h1 and h3"
+        h4, h2 = net.get('h2', 'h4')
+        net.iperf((h2, h4))
+    except:
+        c=1
+
+    
     dumpNodeConnections(net.switches)
     CLI(net)
+    net.stop()
 
 
 if __name__ == '__main__':
